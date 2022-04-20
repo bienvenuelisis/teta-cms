60435a2f1b83f17fa77d28bebdf16efd5d9e9320
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-04-20 23:29:44 +0200

    Update index.dart

--- teta_cms/lib/src/index.dart
+++ teta_cms/lib/src/index.dart
@@ -7,6 +7,7 @@ import 'package:http/http.dart' as http;
 import 'package:socket_io_client/socket_io_client.dart' as socket_io;
 import 'package:teta_cms/src/constants.dart';
 import 'package:teta_cms/src/models/stream_actions.dart';
+import 'package:teta_cms/teta_cms.dart';
 
 part 'components/utils.dart';
 part 'models/no_sql_stream.dart';
