765faa6336b91a66154ab355dcf775c8a0f49b31
Emily <laura.emily.lovi@gmail.com>
2022-05-23 11:38:00 +0200

    fix(auth.dart)

--- teta_cms/lib/src/auth.dart
+++ teta_cms/lib/src/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -7,9 +8,47 @@ import 'package:teta_cms/teta_cms.dart';
 import 'package:universal_platform/universal_platform.dart';
 import 'package:webview_windows/webview_windows.dart';
 import 'package:webviewx/webviewx.dart';
+import 'dart:js';
 
 enum TetaProvider {
   google,
+}
+
+/// Javascript post message model
+class JavascriptPostMessage {
+  /// Constructor
+  JavascriptPostMessage(this.data, this.origin);
+
+  /// Factory constructor
+  factory JavascriptPostMessage.fromJsonString(final String jsonString) {
+    final jsonData = json.decode(jsonString) as Map<String, dynamic>;
+    final data =
+        JavascriptPostMessageData.fromJsonString(jsonData['data'] as String);
+    final origin = jsonData['origin'] as String;
+    return JavascriptPostMessage(data, origin);
+  }
+
+  /// data
+  JavascriptPostMessageData data;
+
+  /// origin
+  String origin;
+}
+
+/// Javascript post message data model
+class JavascriptPostMessageData {
+  /// Constructor
+  JavascriptPostMessageData(this.jwt);
+
+  /// Factory constructor
+  factory JavascriptPostMessageData.fromJsonString(final String jsonString) {
+    final jsonData = json.decode(jsonString) as Map<String, dynamic>;
+    final jwt = jsonData['jwt'] as String;
+    return JavascriptPostMessageData(jwt);
+  }
+
+  /// jwt
+  String jwt;
 }
 
 class TetaAuth {
@@ -51,32 +90,54 @@ class TetaAuth {
   }
 
   Future<bool> signInWithBrowser(
-    final BuildContext context,
+    final BuildContext ctx,
     final int prjId, {
     final TetaProvider provider = TetaProvider.google,
   }) async {
+    late final JsObject child;
+
     final url = await signIn(prjId: prjId, provider: provider);
     TetaCMS.printWarning('Teta Auth return url: $url');
+    final completer = Completer<JavascriptPostMessageData>();
+    Future onParentWindowMessage(final dynamic message) async {
+      if (message == null) return;
+      TetaCMS.printWarning('message: $message');
+      if ((message.origin as String).startsWith('https://auth.teta.so')) {
+        final result =
+            JavascriptPostMessageData.fromJsonString(message.data.toString());
+
+        final token = result;
+
+        //? Do shits with the token here
+
+        child.callMethod('close');
+        completer.complete(result);
+      }
+    }
+
+    context['onmessage'] = onParentWindowMessage;
+    final urls = [url];
+    child = context.callMethod('open', urls) as JsObject;
+
+    return true;
     final windowsController = WebviewController();
     windowsController.url.listen(
       (final url) {
         TetaCMS.printWarning(url);
         if (url.contains('access_token') && url.contains('refresh_token')) {
-          Navigator.of(context, rootNavigator: true).pop(url);
+          Navigator.of(ctx, rootNavigator: true).pop(url);
         }
       },
     );
     WebViewXController? webViewController;
     final result = await showDialog<String>(
-      context: context,
+      context: ctx,
       builder: (final ctx) => AlertDialog(
         backgroundColor: const Color(0xFF181818),
         contentPadding: EdgeInsets.zero,
         content: SizedBox(
-          width:
-              MediaQuery.of(context).size.width >= 600 ? 400 : double.maxFinite,
-          height:
-              MediaQuery.of(context).size.width >= 600 ? 400 : double.maxFinite,
+          width: MediaQuery.of(ctx).size.width >= 600 ? 400 : double.maxFinite,
+          height: MediaQuery.of(ctx).size.width >= 600 ? 400 : double.maxFinite,
           child: Stack(
             children: [
               const Center(
@@ -96,7 +157,7 @@ class TetaAuth {
                     url,
                     permissionKind,
                     isUserInitiated,
-                    context,
+                    ctx,
                   ),
                 )
               else
@@ -114,7 +175,7 @@ class TetaAuth {
                     TetaCMS.printWarning(url);
                     if (url.contains('access_token') &&
                         url.contains('refresh_token')) {
-                      Navigator.of(context, rootNavigator: true).pop(url);
+                      Navigator.of(ctx, rootNavigator: true).pop(url);
                     }
                   },
                 ),
