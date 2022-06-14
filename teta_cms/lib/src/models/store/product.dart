2e20f3c8babc3a4212d9705f60742f31b445fe27
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-06-14 18:09:50 +0200

    feat(product from schema)

--- teta_cms/lib/src/models/store/product.dart
+++ teta_cms/lib/src/models/store/product.dart
@@ -19,6 +19,17 @@ class TetaProduct {
   final bool isPublic;
   final Map<String, dynamic>? metadata;
 
+  TetaProduct fromSchema(final Map<String, String> json) => TetaProduct(
+        id: json['id'] ?? '',
+        name: json['name'] ?? '',
+        price: double.tryParse(json['price'] ?? '') ?? 0,
+        count: int.tryParse(json['count'] ?? '') ?? 0,
+        isPublic: (json['isPublic'] ?? 'false').toLowerCase() == 'true',
+        description: json['description'],
+        image: json['image'],
+        metadata: <String, dynamic>{},
+      );
+
   Map<String, dynamic> toJson() => <String, dynamic>{
         '_id': id,
         'name': name,
