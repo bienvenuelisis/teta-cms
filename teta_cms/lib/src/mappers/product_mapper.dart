d7da8025ba4e3198faca831f4d7ff0496ef61a5c
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-06-14 18:06:05 +0200

    Update product_mapper.dart

--- teta_cms/lib/src/mappers/product_mapper.dart
+++ teta_cms/lib/src/mappers/product_mapper.dart
@@ -18,6 +18,17 @@ class ProductMapper {
         metadata: json['metadata'] as Map<String, dynamic>?,
       );
 
+  TetaProduct fromSchema(final Map<String, String> json) => TetaProduct(
+        id: json['id'] ?? '',
+        name: json['name'] ?? '',
+        price: double.tryParse(json['price'] ?? '') ?? 0,
+        count: int.tryParse(json['count'] ?? '') ?? 0,
+        isPublic: (json['isPublic'] ?? 'false').toLowerCase() == 'true',
+        description: json['description'],
+        image: json['image'],
+        metadata: <String, dynamic>{},
+      );
+
   List<TetaProduct> mapProducts(final List<Map<String, dynamic>> products) =>
       products.map(mapProduct).toList(growable: true);
 }
