34e60bbbeca4c93feebe1d6aae2d162c77f8caf5
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-06-27 19:04:18 +0200

    Revert "Revert "feat(new cms endpoints)""
    This reverts commit e6a6d7eddbb2ad2bc1b6f1ac7d2f9e3d1e2e2dce.

--- teta_cms/lib/src/constants.dart
+++ teta_cms/lib/src/constants.dart
@@ -2,7 +2,7 @@ class Constants {
   static const String docId = '_id';
   static const String prjIdKey = 'prj_id';
 
-  static const String _host = 'public.teta.so';
+  static const String _host = 'cms.teta.so';
   static const String _port = '9840';
   static const String tetaUrl = 'https://$_host:$_port';
   static const Map<String, String> defaultHeaders = <String, String>{
